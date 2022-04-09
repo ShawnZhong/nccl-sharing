7b80e7f51dc3dda2277ffa1872a2071d062b100b
ShawnZhong <github@shawnzhong.com>
2022-04-09 08:44:47 -0400

    minor

--- prof.sh
+++ prof.sh
@@ -9,4 +9,4 @@ make -C "${base_dir}"/nccl -j"$(nproc)"
 mkdir -p "${base_dir}"/build
 cmake -B "${base_dir}"/build -S . -DCMAKE_BUILD_TYPE=Release
 cmake --build "${base_dir}"/build -- -j"$(nproc)"
-nvprof --print-gpu-trace "${base_dir}"/build/nccl_test
+NCCL_P2P_DISABLE=1 NCCL_DEBUG=INFO nvprof --print-gpu-trace "${base_dir}"/build/nccl_test
