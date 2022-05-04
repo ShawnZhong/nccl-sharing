39f6fc277b5c5646f534a437223f5f1e3c5bb723
ShawnZhong <github@shawnzhong.com>
2022-05-04 02:42:27 -0400

    update scripts

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ def print_profiling_info(prof):
 
     comp_time = sum(e.cuda_time_total for e in comp_events) / 1e3
     comm_time = sum(e.cuda_time_total for e in comm_events) / 1e3
-    cuda_time = comp_time + comm_time
 
     overlap_time = 0
     for comm_event in comm_events:
@@ -40,12 +39,11 @@ def print_profiling_info(prof):
             if comm_event.time_range.end < comp_event.time_range.start:
                 continue
             min_end = min(comm_event.time_range.end, comp_event.time_range.end)
-            max_start = max(comm_event.time_range.start, comp_event.time_range.start)
+            max_start = max(comm_event.time_range.start,
+                            comp_event.time_range.start)
             overlap_time += (min_end - max_start) / 1e3
 
-    print(
-        f"comp {comp_time:6.3f} ms, comm {comm_time:6.3f} ms, cuda {cuda_time:6.3f} ms, overlap {overlap_time:6.3f} ms"
-    )
+    print(f"{comp_time:6.3f}, {comm_time:6.3f}, {overlap_time:6.3f}")
 
 
 def train(rank, args):
@@ -78,10 +76,11 @@ def train(rank, args):
 
         if rank == 0 and i == args.niter - 1:
             print_profiling_info(prof)
-        
+
         throughput = args.batch_size / (end_ts - start_ts)
         cpu_time = end_ts - start_ts
         print(f"[{rank}] iter {i}: {throughput:.2f} it/sec, {cpu_time:.3f} sec")
+
 
 def add_common_args(parser):
     parser.add_argument("-w", "--world_size", type=int, default=2)
@@ -89,22 +88,28 @@ def add_common_args(parser):
     parser.add_argument("-d", "--debug", action="store_true")
     parser.add_argument("-g", "--grid", action="store_true")
 
+
 def run_configs(fn, args):
     if args.grid:
-        args.configs = [f"{i},{2 ** j}" for i in range(1, 5) for j in range(6, 10)]
+        args.configs = ["0,0"]
+        args.configs += [f"{i},{2 ** j}" for i in range(1, 5) for j in range(6, 10)]
     print(args)
-    for config in args.configs:
-        nchannels, nthreads = map(int, config.split(","))
-        os.environ["NCCL_P2P_DISABLE"] = "1"
-        os.environ["NCCL_MIN_NCHANNELS"] = str(nchannels)
-        os.environ["NCCL_MAX_NCHANNELS"] = str(nchannels)
-        os.environ["NCCL_NTHREADS"] = str(nthreads)
-        if args.debug:
+    os.environ["NCCL_P2P_DISABLE"] = "1"
+    if args.debug:
             os.environ["TORCH_CPP_LOG_LEVEL"] = "INFO"
             os.environ["TORCH_DISTRIBUTED_DEBUG"] = "DETAIL"
             os.environ["NCCL_DEBUG"] = "INFO"
+    for config in args.configs:
+        nchannels, nthreads = map(int, config.split(","))
+        args.nchannels = nchannels
+        args.nthreads = nthreads
+        if nchannels != 0 and nthreads != 0:
+            os.environ["NCCL_MIN_NCHANNELS"] = str(nchannels)
+            os.environ["NCCL_MAX_NCHANNELS"] = str(nchannels)
+            os.environ["NCCL_NTHREADS"] = str(nthreads)
         print(f"{nchannels},{nthreads:4}, ", end="")
         mp.spawn(fn, args=(args,), nprocs=args.world_size)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
